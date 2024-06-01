b9797c3a58ac69216abb9bca028c3bdc2ddb7819
Karma-1710 <karmahsoni@gmail.com>
2024-06-01 09:20:30 +0530

    Code for Creating and Displaying Linked list

--- LinkedList/01_display_linkedlist.cpp
+++ LinkedList/01_display_linkedlist.cpp
@@ -1,1 +1,44 @@
 #include<iostream>
+using namespace std;
+
+//Creating the structure of a node using class
+class Node{
+    public:
+        int data;
+        Node* next;
+};
+
+
+int main() {
+    int A[] = {3,5,6,7,9,1};
+
+    Node* head = new Node;
+    Node* temp;
+    Node* last;
+
+    head->data = A[0];
+    head->next = nullptr;
+    last = head;
+
+    // Creating a Linked List
+    for(int i=1; i<sizeof(A)/sizeof(A[0]); i++){
+        temp =  new Node;
+        temp->data = A[i];
+        temp->next = nullptr;
+        last->next = temp;
+        last = temp;
+    }
+
+    // Displaying a Linked List
+    Node* p = head;
+    while(p != nullptr){
+        cout << p->data << "->" << flush;
+        p=p->next;
+    }
+
+    return 0;
+}
+
+
+// OUTPUT: 3->5->6->7->9->1
+
