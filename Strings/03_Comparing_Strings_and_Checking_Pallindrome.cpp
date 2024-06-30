56a054d03cd8507f2c64dfdf80bbb92081a30d1a
Karma-1710 <karmahsoni@gmail.com>
2024-06-30 18:34:12 +0530

    Comparing Strings and Checking for Pallindrome

---
+++ Strings/03_Comparing_Strings_and_Checking_Pallindrome.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int CheckPallindrome(char* C){
+    int i=0, j=0;
+    for(j=0;C[j] != '\0';j++){};
+    j = j-1;
+    while(i < j){
+        if(C[i] != C[j]){
+            return 0;
+        }else{
+            i++;
+            j--;
+        }
+    }
+    return 1;
+}
+
+int main(){
+    char A[] = "Painter";
+    char B[] = "Painting";
+    int i, j;
+    for(i=0,j=0;A[i] != '\0' && B[j] != '\0';i++,j++){
+        if(A[i] != B[j]){
+            break;
+        }
+    }
+    if(A[i] == B[j]){
+        cout << "Strings are equal" << endl;
+    } else if(A[i] < B[j]){
+        cout << "String A is Smaller" << endl;
+    } else{
+        cout << "String A is Greater" << endl;
+    }
+
+    char C[] = "MadaMMadaM";
+    cout <<  CheckPallindrome(C) << endl;
+    return 0;
+}
